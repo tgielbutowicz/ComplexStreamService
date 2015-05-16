e87d8dd071d7642534e86ba3247e1fa80a3d0a71
Sandra <sandrolina17@gmail.com>
2015-05-16 17:53:19 +0200

    test Sandra

---
+++ http_page splitter/http_page_splitter.py
@@ -0,0 +1,22 @@
+__author__ = 'Sandra'
+
+
+
+#!/usr/bin/env python
+
+import dpkt
+
+
+f = open(r'C:\Users\Sandra\PycharmProjects\ComplexStreamService\http_page_splitter\test.pcap')
+pcap = dpkt.pcap.Reader(f)
+
+for ts, buf in pcap:
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+
+    if tcp.dport == 80 and len(tcp.data) > 0:
+        http = dpkt.http.Request(tcp.data)
+        print http.uri
+
+f.close()
